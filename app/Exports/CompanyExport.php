2b297f4a647806ce5ca19cbe93f3ea3d14c18a2c
Ankit Kumar. <ankit@webmavens.com>
2024-06-27 14:53:02 +0530

    Implement the WithCustomCsvSettings interface and define the desired settings

--- app/Exports/CompanyExport.php
+++ app/Exports/CompanyExport.php
@@ -4,37 +4,24 @@ namespace App\Exports;
 
 use App\Models\NewCompny;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
 
-class CompanyExport implements FromCollection
+class CompanyExport implements FromCollection, WithCustomCsvSettings
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function headings(): array
-    {
-        return [
-            'Id | Name | Address | Phone',
-        ];
-    }
-
     public function collection()
     {
-        return NewCompny::all()->map(function ($company) {
-            return [
-                $company->id . ' | ' . $company->name . ' | ' . $company->address . ' | ' . $company->Contact_no,
-            ];
-        });
+        return NewCompny::all();
     }
-
-
 
     public function fileName()
     {
         return 'company_' . now()->format('Y-m-d-H-i-s') . '.xlsx';
     }
 
-    // public function fileName()
-    // {
-    //     return 'company_' . now()->format('Y-m-d-H-i-s') . '.csv';
-    // }
+    public function getCsvSettings(): array
+    {
+        return [
+            'delimiter' => '|',
+        ];
+    }
 }
